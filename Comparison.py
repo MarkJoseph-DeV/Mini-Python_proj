2db7c8567634aba54ec84873d56e791d67decd01
Marky <markjosephatwil15@gmail.com>
2025-06-30 10:02:13 +0800

    Add files via upload

---
+++ Comparison.py
@@ -0,0 +1,19 @@
+# This pratice is for documentation only...
+# This code compares two lists and checks if they are equal.
+# If they are equal, it prints the type of the lists; otherwise, it prompts to try again.
+# The lists are defined as tuples of strings.
+# The function `compare_lists` takes two arguments and compares them.
+
+
+x = ('mark,john,paul,ringo') # Define the first list as a tuple of strings
+y = ('john,paul,ringo,mark')
+
+def compare_lists(x, y): #Arrgumentary Sample Code...
+    
+    if x == y():    # Compare the two lists
+        print(type(x and y))
+    else:
+        print("Try Again")
+        
+        #calling the function
+compare_lists
